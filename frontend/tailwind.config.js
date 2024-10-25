df5f1302d0002d843eee153b04ec6604c46c4356
estoericull3nea <palisocericson87@gmail.com>
2024-10-26 07:59:00 +0800

    folder structured

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '2rem',
+        },
+      },
+      colors: {
+        main: '#df0001',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['light'],
+  },
+}
