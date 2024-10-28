a7be70b6c778e16742de042d7c6c2a3a79160de2
“Arabella” <cancinoarabellagrace@gmail.com>
2024-10-29 00:29:20 +0800

    login page UI

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -52,44 +52,7 @@ const Login = () => {
                     required
                   />
                 </div>
-                <div>
-                  <label
-                    htmlFor='confirm-password'
-                    className='block mb-2 text-sm font-medium text-gray-900'
-                  >
-                    Confirm password
-                  </label>
-                  <input
-                    type='password'
-                    name='confirm-password'
-                    id='confirm-password'
-                    placeholder='••••••••'
-                    className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5'
-                    required
-                  />
-                </div>
-                <div className='flex items-start'>
-                  <div className='flex items-center h-5'>
-                    <input
-                      id='terms'
-                      aria-describedby='terms'
-                      type='checkbox'
-                      className='w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300'
-                      required
-                    />
-                  </div>
-                  <div className='ml-3 text-sm'>
-                    <label htmlFor='terms' className='font-light text-gray-500'>
-                      I accept the{' '}
-                      <a
-                        className='font-medium text-primary-600 hover:underline'
-                        href='#'
-                      >
-                        Terms and Conditions
-                      </a>
-                    </label>
-                  </div>
-                </div>
+
                 <button
                   type='submit'
                   className='w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center'
