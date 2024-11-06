f11523637f3ac9b6f3ada59ba1c4700a208f3c86
Ericson <palisocericson87@gmail.com>
2024-11-06 14:25:45 +0800

    user can't view profile password input

--- frontend/src/Pages/UserProfile/UserInfo.jsx
+++ frontend/src/Pages/UserProfile/UserInfo.jsx
@@ -193,24 +193,26 @@ const UserInfo = ({ isVisitor }) => {
           />
         </div>
 
-        <div>
-          <label
-            htmlFor='password'
-            className='block mb-2 text-sm font-medium text-gray-900'
-          >
-            Password
-          </label>
-          <input
-            type='password'
-            name='password'
-            id='password'
-            className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5'
-            placeholder='••••••••'
-          />
-          <p className='text-sm text-gray-500 mt-1'>
-            If you don't want to change your password, leave this field empty.
-          </p>
-        </div>
+        {!isVisitor && (
+          <div>
+            <label
+              htmlFor='password'
+              className='block mb-2 text-sm font-medium text-gray-900'
+            >
+              Password
+            </label>
+            <input
+              type='password'
+              name='password'
+              id='password'
+              className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5'
+              placeholder='••••••••'
+            />
+            <p className='text-sm text-gray-500 mt-1'>
+              If you don't want to change your password, leave this field empty.
+            </p>
+          </div>
+        )}
 
         {!isVisitor && (
           <button
