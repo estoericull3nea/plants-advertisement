a3f2a5213f17a08d8262a3a7fc97548659fa9027
Ericson <palisocericson87@gmail.com>
2024-11-16 15:17:52 +0800

    updated

--- backend/api/controllers/payment.controller.js
+++ backend/api/controllers/payment.controller.js
@@ -4,13 +4,11 @@ import {
 } from '../services/payment.service.js'
 import mongoose from 'mongoose'
 
-// Controller to create and confirm payment intent using the user's cart total
 export const createPayment = async (req, res) => {
-  let { userId, paymentMethodId } = req.body // Expecting userId and paymentMethodId in the body
+  let { userId, paymentMethodId } = req.body
 
   console.log(req.body)
 
-  // Validate inputs
   if (!userId || !paymentMethodId) {
     return res.status(400).json({
       success: false,
@@ -18,22 +16,18 @@ export const createPayment = async (req, res) => {
     })
   }
 
-  // If the userId is a number, cast it to an ObjectId
   if (typeof userId === 'number') {
-    userId = mongoose.Types.ObjectId(userId.toString()) // Convert number to ObjectId
+    userId = mongoose.Types.ObjectId(userId.toString())
   }
 
   try {
-    // Step 1: Create the payment intent using the total amount from the cart
     const paymentIntent = await createPaymentIntent(userId)
 
-    // Step 2: Confirm the payment using the paymentMethodId provided
     const paymentConfirmation = await confirmPayment(
       paymentIntent.id,
       paymentMethodId
     )
 
-    // Check the status of the payment
     if (paymentConfirmation.status === 'succeeded') {
       return res.status(200).json({
         success: true,
