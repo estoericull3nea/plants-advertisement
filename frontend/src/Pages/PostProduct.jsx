90053068cadbee48cb86ed82c2ba6982f86553c6
Ericson <palisocericson87@gmail.com>
2024-11-17 23:57:18 +0800

    updated

--- frontend/src/Pages/PostProduct.jsx
+++ frontend/src/Pages/PostProduct.jsx
@@ -280,6 +280,7 @@ const PostProduct = () => {
                 type='file'
                 id='images'
                 multiple
+                required
                 accept='image/*'
                 onChange={handleImageChange}
                 className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5'
